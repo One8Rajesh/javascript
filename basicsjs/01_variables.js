cb05555b7eecc20fec5ca237e20fdac9012bc4fd
Rajes Rauniyar <rajeshbro188@gmail.com>
2025-03-24 19:56:46 +0000

    basicjs

---
+++ basicsjs/01_variables.js
@@ -0,0 +1,15 @@
+// const stands for constant it usely use when we want our value not to be change in whole program at that time we use const variables
+const AccountId=456996
+
+// we use let variable to let something if we want to change the value anywhere in program .it modifies the values
+let Name="Rajesh Rauniyar"
+
+// dont use var its old 
+var email="raj@gmail.com"
+
+// if we dont provide variable it automatically by default run
+accountstate="saving"
+
+
+console.log(AccountId);
+console.table([AccountId, Name,email, accountstate])
