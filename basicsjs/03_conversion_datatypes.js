55142dbf88c9f517537e1cd5e837927af4a474b8
Rajes Rauniyar <rajeshbro188@gmail.com>
2025-03-24 20:39:21 +0000

    conversion_datatypes

---
+++ basicsjs/03_conversion_datatypes.js
@@ -0,0 +1,20 @@
+// we can change the data types simply by somehow this way .we can change any data type in any data types
+let Name="rajesh"
+let Namechange= Number(Name)
+console.log(Namechange)
+console.log(typeof Namechange)
+
+let num=1
+let numchange= Boolean(num)
+console.log(numchange)
+console.log(typeof numchange)
+
+let bool=true
+let boolchange= Number(bool)
+console.log(boolchange)
+console.log(typeof boolchange)
+
+let string="rajesh"
+let stringchange= Boolean(string)
+console.log(stringchange)
+console.log(typeof stringchange)
