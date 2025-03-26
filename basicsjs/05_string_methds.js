0c7c4e7cddb77ef3aab88571c8a9a4855ed7a1b8
Rajes Rauniyar <rajeshbro188@gmail.com>
2025-03-26 19:50:59 +0000

    strings methos

---
+++ basicsjs/05_string_methds.js
@@ -0,0 +1,32 @@
+const namee= "Rajesh rauniyar"
+
+// find the lennth of string
+console.log(namee.length);
+
+// convert string into uppercase
+console.log(namee.toUpperCase());
+
+// use to find the character string at which place
+console.log(namee.charAt(2));
+
+// use to find the string index where it is located
+console.log(namee.indexOf('n'));
+
+// find the string between the given size
+console.log(namee.substring(0,5));
+
+const newname="  rajesh  "
+
+// remove the space and print
+console.log(newname.trim());
+
+const url='rajeshbro188/google/ceo.in'
+
+// use to replace the things we want to replace in our string
+console.log(url.replace('/','_'))
+
+// make the array content of the string
+console.log(url.split('_'))
+
+// check it occurs or not in main string,if its occur it return true otherwise false
+console.log(url.includes('moon'))
