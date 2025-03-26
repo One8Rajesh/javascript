1576855724b0d9b9165d3b46bf8fa84dde763458
Rajes Rauniyar <rajeshbro188@gmail.com>
2025-03-26 19:02:18 +0000

    string

---
+++ basicsjs/string.js
@@ -0,0 +1,8 @@
+const name="Rajesh Rauniyar"
+const prn= 24070122277
+
+// this method of printing the result is out dated so we use some new method
+// console.log(name +  prn + " done");
+
+// this is the new method to printing the result of string
+console.log(`my name is ${name} and my prn is ${prn} and I'm in 1st year`)
